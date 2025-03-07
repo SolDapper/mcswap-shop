efa5d55c7788dc61d50baf9bf8e66af6d40ebd41
SolDapper <soldapper.com@gmail.com>
2025-03-07 17:58:39 -0500

    modified:   mcswap-market.js 	modified:   package.json

--- mcswap-market.js
+++ mcswap-market.js
@@ -7,6 +7,8 @@ import Toastify from 'toastify-js';
 import "toastify-js/src/toastify.css";
 import parcelPluginStaticFilesCopy from "parcel-plugin-static-files-copy";
 import mcswap from "mcswap-sdk";
+import "mcswap-sdk/mcswap-market.css";
+
 // import {PublicKey,Keypair,Connection,TransactionInstruction,SystemProgram,SYSVAR_INSTRUCTIONS_PUBKEY} from "@solana/web3.js";
 // import BufferLayout from "buffer-layout";
 // import * as splToken from "@solana/spl-token";
